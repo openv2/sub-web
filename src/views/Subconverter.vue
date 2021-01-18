4e50bbc68559eb52bfe4ddff6a284e67e173a71d
openv2 <65837917+openv2@users.noreply.github.com>
2021-01-18 15:29:39 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -4,7 +4,7 @@
       <el-col>
         <el-card>
           <div slot="header">
-            订阅转换
+            OpenV2订阅转换
             <svg-icon icon-class="github" style="margin-left: 20px" @click="goToProject" />
             <svg-icon icon-class="telegram" style="margin-left: 20px" @click="gotoTgChannel" />
 
